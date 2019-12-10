365991599d734fe01d4fdcea064f9703b5a825f2
Julia <56934563+evisgood@users.noreply.github.com>
2019-12-10 19:10:36 +0200

    fibo recursive

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 9; i++)
+            System.out.printf("Fibonacci of %d is: %d\n", i, fiboRec(i));
+    }
+
+            public static long fiboRec(long number) {
+
+                if ((number == 0) || (number == 1))
+                    return number;
+                else
+                    return fiboRec(number - 1) + fiboRec(number - 2);
+            }
+
+
+        }# examminor
